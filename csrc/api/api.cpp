6be2aa58c8e56e7c3fee1d74ac9b606ec0e48692
PoohTheWinnie <winston.x.cai@gmail.com>
2026-09-21 01:50:28 +0800

    Use positional binding wrapper for decode

--- csrc/api/api.cpp
+++ csrc/api/api.cpp
@@ -21,59 +21,52 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
     m.doc() = "FlashMLA";
     m.def(
         "sparse_decode_fwd",
-        [](const at::Tensor &q,
-           const at::Tensor &kv,
-           const at::Tensor &indices,
-           const pybind11::object &topk_length,
-           const pybind11::object &attn_sink,
-           const pybind11::object &tile_scheduler_metadata,
-           const pybind11::object &num_splits,
-           const pybind11::object &extra_kv,
-           const pybind11::object &extra_indices,
-           const pybind11::object &extra_topk_length,
-           int d_v,
-           float sm_scale) {
-            auto topk_length_opt = optional_tensor(topk_length);
-            auto attn_sink_opt = optional_tensor(attn_sink);
-            auto tile_scheduler_metadata_opt = optional_tensor(tile_scheduler_metadata);
-            auto num_splits_opt = optional_tensor(num_splits);
-            auto extra_kv_opt = optional_tensor(extra_kv);
-            auto extra_indices_opt = optional_tensor(extra_indices);
-            auto extra_topk_length_opt = optional_tensor(extra_topk_length);
+        [](const pybind11::args &args) {
+            if (args.size() != 12) {
+                throw pybind11::type_error("sparse_decode_fwd expects 12 positional arguments");
+            }
+            auto q = args[0].cast<at::Tensor>();
+            auto kv = args[1].cast<at::Tensor>();
+            auto indices = args[2].cast<at::Tensor>();
+            auto topk_length_opt = optional_tensor(args[3]);
+            auto attn_sink_opt = optional_tensor(args[4]);
+            auto tile_scheduler_metadata_opt = optional_tensor(args[5]);
+            auto num_splits_opt = optional_tensor(args[6]);
+            auto extra_kv_opt = optional_tensor(args[7]);
+            auto extra_indices_opt = optional_tensor(args[8]);
+            auto extra_topk_length_opt = optional_tensor(args[9]);
             return sparse_attn_decode_interface(
                 q, kv, indices, topk_length_opt, attn_sink_opt,
                 tile_scheduler_metadata_opt, num_splits_opt, extra_kv_opt,
-                extra_indices_opt, extra_topk_length_opt, d_v, sm_scale);
+                extra_indices_opt, extra_topk_length_opt,
+                args[10].cast<int>(), args[11].cast<float>());
         });
     m.def(
         "remnant_sparse_decode_fwd",
-        [](const at::Tensor &q,
-           const at::Tensor &kv,
-           const at::Tensor &indices,
-           const pybind11::object &topk_length,
-           const pybind11::object &attn_sink,
-           const pybind11::object &tile_scheduler_metadata,
-           const pybind11::object &num_splits,
-           const pybind11::object &extra_indices,
-           const pybind11::object &extra_topk_length,
-           const at::Tensor &remnant_values,
-           const at::Tensor &remnant_bitmaps,
-           const at::Tensor &remnant_scales,
-           const at::Tensor &remnant_raw_indices,
-           const at::Tensor &remnant_freqs,
-           int d_v,
-           float sm_scale) {
-            auto topk_length_opt = optional_tensor(topk_length);
-            auto attn_sink_opt = optional_tensor(attn_sink);
-            auto tile_scheduler_metadata_opt = optional_tensor(tile_scheduler_metadata);
-            auto num_splits_opt = optional_tensor(num_splits);
-            auto extra_indices_opt = optional_tensor(extra_indices);
-            auto extra_topk_length_opt = optional_tensor(extra_topk_length);
+        [](const pybind11::args &args) {
+            if (args.size() != 16) {
+                throw pybind11::type_error("remnant_sparse_decode_fwd expects 16 positional arguments");
+            }
+            auto q = args[0].cast<at::Tensor>();
+            auto kv = args[1].cast<at::Tensor>();
+            auto indices = args[2].cast<at::Tensor>();
+            auto topk_length_opt = optional_tensor(args[3]);
+            auto attn_sink_opt = optional_tensor(args[4]);
+            auto tile_scheduler_metadata_opt = optional_tensor(args[5]);
+            auto num_splits_opt = optional_tensor(args[6]);
+            auto extra_indices_opt = optional_tensor(args[7]);
+            auto extra_topk_length_opt = optional_tensor(args[8]);
+            auto remnant_values = args[9].cast<at::Tensor>();
+            auto remnant_bitmaps = args[10].cast<at::Tensor>();
+            auto remnant_scales = args[11].cast<at::Tensor>();
+            auto remnant_raw_indices = args[12].cast<at::Tensor>();
+            auto remnant_freqs = args[13].cast<at::Tensor>();
             return sparse_attn_decode_remnant_interface(
                 q, kv, indices, topk_length_opt, attn_sink_opt,
                 tile_scheduler_metadata_opt, num_splits_opt, extra_indices_opt,
                 extra_topk_length_opt, remnant_values, remnant_bitmaps,
-                remnant_scales, remnant_raw_indices, remnant_freqs, d_v, sm_scale);
+                remnant_scales, remnant_raw_indices, remnant_freqs,
+                args[14].cast<int>(), args[15].cast<float>());
         });
     m.def("dense_decode_fwd", &dense_attn_decode_interface);
     m.def("sparse_prefill_fwd", &sparse_attn_prefill_interface);
